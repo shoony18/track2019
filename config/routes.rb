d16accd3eb8fc98bfa3227302bc940c6ef9e658e
刈田修平 <karitashuhei@karitashuuheinoMacBook-puro.local>
2019-04-15 23:35:17 +0900

    make it better

--- config/routes.rb
+++ config/routes.rb
@@ -35,8 +35,7 @@ Rails.application.routes.draw do
 
   get "/" => "home#top"
   get "/about" => "home#about"
-  root "home#top"
-
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
